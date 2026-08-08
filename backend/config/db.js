9728c09c985fc489b55301d5ce1695c2c8d1f3be
Kenieboy <syntax0330@gmail.com>
2026-08-08 23:21:27 +0800

    .env and db

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,13 +5,14 @@ dotenv.config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
